1f4bccf463f438bfcecba8e17aeb09a247ea6a7b
Sarah A. Abu Zeneh <sara.abuzena2000@gmail.com>
2021-09-16 00:46:57 +0300

    ex 1

--- third-react-app/src/App.js
+++ third-react-app/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import Information  from './Data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const url = 'https://jsonplaceholder.typicode.com/posts';
+    const [data, getData] = useState("");
+    const getAllData = () =>{
+      axios.get(url).then(response=>{
+        const allData= response;
+        getData(allData);
+      }).catch(err=>alert(err))
+    }
+    useEffect(() => {
+      getAllData()
+      console.log({data})
+
+    })
+    
+    return (<div>
+      <Information data={data}/>           
+      
+    </div>)
 }
 
 export default App;
---
+++ third-react-app/src/Data.js
@@ -0,0 +1,4 @@
+const Information = (props) =>{
+    return<></>
+}
+export default Information
