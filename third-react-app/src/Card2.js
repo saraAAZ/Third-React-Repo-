cf85486f245a9f740f66705517d46964a82a6e6c
Sarah A. Abu Zeneh <sara.abuzena2000@gmail.com>
2021-09-16 22:45:00 +0300

    Solution Of EX2

---
+++ third-react-app/src/Card2.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+
+  const MediaCard = (props) =>{
+      let {data} = props;
+      return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={data.picture}
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {data.title}
+        </Typography>
+        <br></br>
+        <Typography>
+        Fisrt Name : {data.firstName}s
+        </Typography>
+        <Typography>
+            Last Name : {data.lastName};
+        </Typography>
+      </CardContent>
+
+    </Card>
+  );
+}
+export default  MediaCard
